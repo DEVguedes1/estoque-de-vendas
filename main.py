8f1fd298a07129ca8741d4cbfbcac7da76e14e13
Nicolas <nicolasguedesguedes081@gmail.com>
2025-07-25 23:00:39 -0300

    organização

---
+++ main.py
@@ -0,0 +1,8 @@
+#sistema_vendas/
+
+# ├── main.py                   # Ponto de entrada do programa
+# ├── estoque/
+# │   ├── __init__.py           # Torna 'estoque' um pacote
+# │   ├── gerenciador.py        # Funções para carregar, salvar e adicionar produtos
+# │   └── vendas.py             # Função para registrar venda
+# └── estoque.txt               # Arquivo de dados
