be049149b1336226b66506a208a328d509b8c2ef
Nicolas <nicolasguedesguedes081@gmail.com>
2025-07-25 23:16:54 -0300

    carregar estoque

---
+++ estoque/gerenciador.py
@@ -0,0 +1,30 @@
+import os
+
+arquivo_do_estoque = 'estoque.txt'
+
+def carregar_estoque():
+    estoque={}
+    if not os.path.exists(arquivo_do_estoque):
+        return estoque 
+    with open (arquivo_do_estoque,'r') as f:
+        for linha in f:
+            partes = linha.strip().split(',')
+            if len(partes)==5:
+                id_produto = int(partes[0])
+                nome_do_produto = partes[1]
+                quantidades = int(partes[2])
+                valor_compra = float(partes[3])
+                preco_venda = float(partes[4])
+                estoque[id_produto]={
+                    "nome do produto":nome_do_produto,
+                    "quantidade":quantidades,
+                    "valor da compra":valor_compra,
+                    "preço da venda":preco_venda
+                }
+    return estoque
+                
+def salvar_estoque():
+    
+
+def adicionar_produto():
+    return
